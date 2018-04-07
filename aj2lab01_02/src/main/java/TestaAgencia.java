7e90e2af33144decdf1280d5393049875aa11708
Felipe Leal <36779282+felipeleal94@users.noreply.github.com>
2018-04-07 12:42:38 -0300

    Add files via upload

---
+++ aj2lab01_02/src/main/java/TestaAgencia.java
@@ -0,0 +1,16 @@
+/*
+ * Globalcode - "The Developers Company"
+ * 
+ * Academia do Java
+ */
+class TestaAgencia {
+
+    public static void main(String[] args) {
+        // Criacao da agencia
+        // Inicializacao da agencia
+        // Impressao dos dados da agencia
+        Agencia a = new Agencia();
+        a.inicializaAgencia("Felipe e EU",1328167386);
+        a.imprimeDados();
+    }
+}
---
+++ aj2lab01_02/src/main/java/TestaCliente.java
@@ -0,0 +1,17 @@
+/*
+ * Globalcode - "The Developers Company"
+ * 
+ * Academia do Java
+ */
+class TestaCliente {
+
+    public static void main(String[] args) {
+        // Criacao do cliente
+        // Inicializacao do cliente
+        // Impressao dos dados do cliente
+        Cliente c = new Cliente();
+        c.inicializaCliente("Felipe e EU", "123713721379");
+        c.imprimeDados();
+        
+    }
+}
